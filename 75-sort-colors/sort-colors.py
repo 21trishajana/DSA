423ffba7ada1175399f90ae9e59a1f444e24d01f
trisha <21trisha2005@gmail.com>
2026-07-19 12:07:38 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (21.47%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -6,20 +6,21 @@ class Solution:
         count0 = 0
         count1 = 0
         count2 = 0
-        for i in range(len(nums)):
-            if nums[i] == 0:
+        for i in nums:
+            if i == 0:
                 count0 += 1
-            elif nums[i] == 1:
+            elif i == 1:
                 count1 += 1
             else:
                 count2 += 1
         j = 0
-        for k in range(count0):
+        for _ in range(count0):
             nums[j] = 0
             j += 1
-        for k in range(count1):
+        for _ in range(count1):
             nums[j] = 1
             j += 1
-        for k in range(count2):
+        for _ in range(count2):
             nums[j] = 2
             j += 1
+        return nums
