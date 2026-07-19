54ceca83650f06f50d4ddb9de664a8d709193c2a
trisha <21trisha2005@gmail.com>
2026-07-19 11:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (95.57%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,14 +3,14 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        r = m + n - 1
-        i = m-1
-        j = n-1
+        i = m - 1
+        j = n - 1
+        k = m + n -1
         while j >= 0:
             if i >= 0 and nums1[i] > nums2[j]:
-                nums1[r] = nums1[i]
+                nums1[k] = nums1[i]
                 i -= 1
             else:
-                nums1[r] = nums2[j]
+                nums1[k] = nums2[j]
                 j -= 1
-            r -= 1
+            k -= 1
